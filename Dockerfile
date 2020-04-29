6e69182ad719c17104f629b0a34a8f9dfadfc37f
Mehdi Abaakouk <sileht@sileht.net>
2020-04-29 17:16:15 +0200

    Change command used to connect to mongo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3-alpine
 RUN pip install flask pymongo
 COPY app.py /
+COPY rds-combined-ca-bundle.pem /
+WORKDIR /
 ENV FLASK_APP /app.py
 ENTRYPOINT ["flask", "run", "-h", "0.0.0.0", "-p", "80"]
 EXPOSE 80
--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pymongo
 import flask
 
 client = pymongo.MongoReplicaSetClient(os.environ["QOVERY_DATABASE_MY_DDB_CONNECTION_URI"])
-db_name = client.get_default_database().name
+print("Mongo info:")
+print(client.server_info())
 
 app = flask.Flask()
 
 @app.route("/")
 def index():
-    return f"It works, database name is {db_name}"
+    return f"It works"
