5ffac29287072df3f3e21ac42eb3911ee1d015af
Pierre Mavro <pmavro@qovery.com>
2020-05-27 16:58:36 +0200

    fix: pgready is not enough

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+export PGPASSWORD=$PG_PASSWORD
+
 pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
+
+psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST
 if [ $? -eq 0 ] ; then
     sleep infinity
 fi
