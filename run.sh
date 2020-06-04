6b96d11bc98cd4d861d255e9eb463d3013df5c33
Mehdi Abaakouk <sileht@sileht.net>
2020-06-04 11:28:20 +0200

    typo

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
 echo "Trying select 1"
 psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
 if [ $? -eq 0 ] ; then
-    echo "Everything work, open port 123."
+    echo "Everything work, open port 1234."
     socat -v tcp-l:1234,fork exec:'/bin/cat'
 fi
 exit 1
