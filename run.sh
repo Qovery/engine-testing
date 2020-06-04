245947502b3b6098e917671f7abed9a1b25b61b6
Mehdi Abaakouk <sileht@sileht.net>
2020-06-04 11:09:30 +0200

    open port 1234 for container probe

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 export PGPASSWORD=$PG_PASSWORD
 
+echo "Waiting for postgres"
 pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
 
-psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST
+echo "Trying select 1"
+psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
 if [ $? -eq 0 ] ; then
-    sleep infinity
+    echo "Everything work, open port 123."
+    socat -v tcp-l:1234,fork exec:'/bin/cat'
 fi
+exit 1
