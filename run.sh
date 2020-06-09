0b6c9acce59727f6e1419deac6abc0c014dbba7b
Mehdi Abaakouk <sileht@sileht.net>
2020-06-09 11:20:22 +0200

    feat: fix http server

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 export PGPASSWORD=$PG_PASSWORD
 
 echo "Waiting for postgres"
-pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
+#pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
 
 echo "Trying select 1"
-psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
+#psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
 if [ $? -eq 0 ] ; then
     echo "Everything work, open port 1234."
-    socat -v tcp-l:1234,fork exec:'/bin/cat'
+    mini_httpd -D -d / -p 1234
 fi
 exit 1
