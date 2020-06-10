680550d1937b3f90551849c0da8f77c39916913b
Mehdi Abaakouk <sileht@sileht.net>
2020-06-10 17:43:00 +0200

    fix: uncomment pg testing

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 export PGPASSWORD=$PG_PASSWORD
 
 echo "Waiting for postgres"
-#pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
+pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
 
 echo "Trying select 1"
-#psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
+psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
 if [ $? -eq 0 ] ; then
     echo "Everything work, open port 1234."
     mini_httpd -D -d / -p 1234
