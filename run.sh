99180a8eb2f92fa8f7e934c69ee6bb650cebc0bf
Pierre Mavro <pmavro@qovery.com>
2020-11-04 00:41:55 +0100

    feat: new test with long boot apps

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ pg_isready -d $PG_DBNAME -h $PG_HOST -p $PG_PORT -U $PG_USERNAME
 echo "Trying select 1"
 psql -v ON_ERROR_STOP=1 --username $PG_USERNAME --dbname $PG_DBNAME -h $PG_HOST -c "select 1"
 if [ $? -eq 0 ] ; then
-    echo "Everything work, open port 1234."
+    echo "Everything work, sleeping 5 min"
+    sleep 300
+    echo "open port 1234."
     mini_httpd -D -d / -p 1234
 fi
 exit 1
