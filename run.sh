7ec522238d725c74dd0c391b50ffeff30731ad98
Pierre Mavro <pmavro@qovery.com>
2020-11-19 10:54:23 +0100

    feat: adding new debug functions and refactored code

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ if [ ! -z $ENABLE_DEBUG ] ; then
     set -x
 fi
 
+if [ ! -z $DEBUG_PAUSE ] ; then
+    sleep infinity
+fi
+
 wait_for_redis() {
   for i in `seq 20` ; do
     nc -z "$REDIS_HOST" "$REDIS_PORT" > /dev/null 2>&1
@@ -17,15 +21,18 @@ wait_for_redis() {
   exit 1
 }
 
+ensure_resquest_ok() {
+  echo "Trying connect to redis"
+  redis-cli -u "redis://redis:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}" "info"
+  if [ $? -ne 0 ] ; then
+    echo "Error while trying to get redis info"
+    exit 1
+  fi
+}
 
 echo "Waiting for redis"
 wait_for_redis
+ensure_resquest_ok
 
-
-echo "Trying connect to redis"
-redis-cli -u "redis://redis:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}" "info"
-if [ $? -eq 0 ] ; then
-    echo "Everything work, open port 1234."
-    mini_httpd -D -d / -p 1234
-fi
-exit 1
+echo "Everything work, open port 1234."
+mini_httpd -D -d / -p 1234
